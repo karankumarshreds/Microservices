a4bc07ceb7f8b775951852a16e672567c6bb3380
Karan Kumar <nailedITnerd@gmail.com>
2020-06-20 00:15:57 +0530

    added event bus endpoint

---
+++ event-bus/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.post('/events', (req,res) => {
+    const event = req.body;
+    
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+
+    res.send({status: "OK"});
+});
+
+app.listen(4005, () => {
+    console.log('listening on 4005');
+});
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,12 +14,19 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
     posts[id] = {
         id, title
     };
+    //send the event to event bus as well
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    });
     res.status(200).send(posts[id]);
 });
 
